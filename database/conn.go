215b3ab95fd525e32b35a8d8d9384707dfc2620e
Lucas Henrique Teixeira <lucashenrique-teixeira@hotmail.com>
2018-10-01 03:27:18 +0000

    Add Conection database

---
+++ database/conn.go
@@ -0,0 +1,20 @@
+package database
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type db struct {
+	db *sql.DB
+}
+
+func (d *db) conn() {
+	db, err := sql.Open("mysql", "root:@/contact_api")
+	if err != nil {
+		log.Fatal(err)
+	}
+	d.db = db
+}
