6b01761a62071024cd1d3ea20ec2c98246c7281f
Lucas Henrique <lucas.coelho@oi.net.br>
2018-10-09 13:35:56 -0300

    trying to improve structure

--- database/error.go
+++ database/error.go
@@ -4,9 +4,17 @@ import (
 	"log"
 )
 
-// Check error ...
+// Check error default ...
 func Check(e error) {
 	if e != nil {
 		log.Fatal("\nError => ", e)
 	}
+}
+
+// CheckError return error
+func CheckError(e error) error {
+	if e != nil {
+		return e
+	}
+	return nil
 }
--- database/query.go
+++ database/query.go
@@ -1,38 +1,50 @@
 package database
 
-import "github.com/lucashtc/contact_api/app"
+import (
+	"github.com/lucashtc/contact_api/app"
+)
 
+// GetAll ..
 func (d *Db) GetAll() []app.Contact {
 	var c []app.Contact
 	return c
 }
 
+// GetContact ...
 func (d *Db) GetContact(id int) app.Contact {
 	var c app.Contact
 	return c
 }
 
+// CreateContact ...
 func (d *Db) CreateContact(c app.Contact) error {
 	return nil
 }
 
+// EditContact ...
 func (d *Db) EditContact(c app.Contact) error {
 	return nil
 }
 
 // DeleteContact ...
-func (d *Db) DeleteContact(id int) int64 {
+func (d *Db) DeleteContact(id int) (int64, error) {
 	d.Conn()
 	defer d.db.Close()
 
 	stmt, err := d.db.Prepare("DELETE user where id=?")
-	Check(err)
+	if err != nil {
+		return 0, err
+	}
 
 	res, err := stmt.Exec(id)
-	Check(err)
+	if err != nil {
+		return 0, err
+	}
 
 	qtd, err := res.RowsAffected()
-	Check(err)
+	if err != nil {
+		return 0, err
+	}
 
-	return qtd
+	return qtd, nil
 }
